bd56289381441ef493db92b18b7e36819799fdb8
jose-angel200090 <jose-angel200090@hotmail.com>
2024-10-05 10:12:42 -0500

    ec2 y security group

--- ec2.tf
+++ ec2.tf
@@ -2,8 +2,9 @@ resource "aws_instance" "ec2_pub1" {
   ami                         = "ami-0d081196e3df05f4d"
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.public_subnet_1.id
-  key_name                    = "cloud"
+  key_name                    = "cloud2"
   associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.allow_ssh_http.id]
 
   tags = {
     Name = "ec2_pub1"
@@ -14,8 +15,9 @@ resource "aws_instance" "ec2_pub2" {
   ami                         = "ami-0d081196e3df05f4d"
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.public_subnet_2.id
-  key_name                    = "cloud"
+  key_name                    = "cloud2"
   associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.allow_ssh_http.id]
 
   tags = {
     Name = "ec2_pub2"
